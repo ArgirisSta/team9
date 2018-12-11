0f7cb6e68455881a354cd9d2f6bb9109566bc668
Argiris <argyrios.stamatopoulos@gmail.com>
2018-12-12 01:39:22 +0200

    .

--- src/main/java/com/team9/project/controller/AdminController.java
+++ src/main/java/com/team9/project/controller/AdminController.java
@@ -1,13 +1,23 @@
 package com.team9.project.controller;
 
 import com.team9.project.domain.Person;
+import com.team9.project.form.RegisterForm;
+import com.team9.project.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
 
     @GetMapping(value = "/admin")
     public String showAdminPage() {
@@ -20,8 +30,14 @@ public class AdminController {
     }
 
     @PostMapping(value = "/admin/register")
-    public String handleRegistrationForm(Model model, Person person) {
-        model.addAttribute("person", person);
+    public String handleRegistrationForm(Model model, @Valid @ModelAttribute("registerForm") RegisterForm registerForm,
+                                         BindingResult bindingResult, Person person) {
+        if(!bindingResult.hasErrors()) {
+            model.addAttribute("person", person);
+            adminService.registerPerson(person);
+            return "registered";
+        }
+
         return "index";
     }
 
--- src/main/java/com/team9/project/service/AdminServiceImp.java
+++ src/main/java/com/team9/project/service/AdminServiceImp.java
@@ -3,7 +3,9 @@ package com.team9.project.service;
 import com.team9.project.domain.Person;
 import com.team9.project.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminServiceImp implements AdminService {
     @Autowired
     PersonRepository personRepository;
